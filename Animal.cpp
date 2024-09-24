5277dff355669151c0dddf83ba74b873b4e9e69f
Rakin Bhuyan <142949864+RakinAB@users.noreply.github.com>
2024-09-24 10:26:10 -0500

    commit part 1

---
+++ Animal.cpp
@@ -0,0 +1,61 @@
+#include "Animal.h"
+#include <iostream>
+
+//CLASS ANIMAL
+std::string Animal::displayInfo(){
+    std::cout << "Name: " << getName() << " Age: " << getAge() << std::endl;
+    std::cout << "Type: " << getType() << std::end;
+}
+
+//Getters and Setters : ANIMAL
+std::string getName(){
+    return name;
+}
+
+void Animal::setName(string n){
+    name = n;
+}
+
+int Animal::getAge(){
+    return age;
+}
+
+void Animal::setAge(int years){
+    age = years;
+}
+
+std::string getType(){
+    return type;
+}
+
+void Animal::setType(string t){
+    type = t;
+}
+
+//SUBCLASS DOG AND CAT
+std::string Dog::displayInfo(){
+    std::cout << "Name: " << getName() << " Age: " << getAge() << std::endl;
+    std::cout << "Type: " << getType() <<  " Breed: " << getBreed() << std::endl;
+}
+
+std::string Cat::displayInfo(){
+    std::cout << "Name: " << getName() << " Age: " << getAge() << std::endl;
+    std::cout << "Type: " << getType() <<  " Breed: " << getBreed() << std::endl;
+}
+
+//Getters and Setters: Cat and Dog
+
+std::string Dog::getBreed(){
+    return breed;
+}
+std::string Cat::getBreed(){
+    return breed;
+}
+
+void Dog::setBreed(std::string br){
+    breed = br;
+}
+
+void Cat::setBreed(std::string br){
+    breed = br;
+}
---
+++ Animal.h
@@ -0,0 +1,45 @@
+#ifndef _ANIMAL_H
+#define _ANIMAL_H
+
+#include <string>
+class Animal{
+    protected:
+        Animal();
+        std::string name;
+        int age;
+        std::string type;
+
+    public:
+        void displayInfo();
+        
+        std::string getType();
+        void setType(std::string t);
+
+        std::string getName();
+        void setName(std::string n);
+
+        int getAge();
+        void setAge(int years);
+}
+
+class Dog : public Animal(){
+    public:
+
+        Dog(std::string breed);
+        //Override Animal's displayInfo, will include the breed
+        void displayInfo();
+
+        std::string getBreed();
+        void setBreed();
+}
+
+class Cat : public Animal(){
+    public:
+        Cat(std::string breed);
+
+        //Override Animal's displayInfo, will include the breed
+        void displayInfo()
+        std::string getBreed();
+        void setBreed();
+}
+#endif
