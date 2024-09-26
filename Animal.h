a33ff563c4ddf3d18b4bd731f91cd78c1332f6fa
Rakin Bhuyan <142949864+RakinAB@users.noreply.github.com>
2024-09-26 10:19:14 -0500

    commit Part1 edits

--- Animal.h
+++ Animal.h
@@ -23,7 +23,7 @@ class Animal{
         void setAge(int years);
 };
 
-class Dog : public Animal(){
+class Dog : public Animal{
     public:
 
         Dog(std::string n, int a, std::string t, std::string breed);
@@ -34,7 +34,7 @@ class Dog : public Animal(){
         void setBreed();
 };
 
-class Cat : public Animal(){
+class Cat : public Animal{
     public:
         Cat(std::string n, int a, std::string t, std::string breed);
 
